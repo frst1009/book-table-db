3fecfd156cf16b54cdfe3efeb0db896b6092c624
frst1009 <fttkpsttkp@gmail.com>
2023-06-07 12:45:15 +0400

    login page

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,14 @@
-import BookTable from "./table";
+import Login from "./pages/Login";
+import BookTable from "./pages/table";
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-     <BookTable/>
-    </div>
+    <BrowserRouter>
+  <Routes>
+  <Route path="/books" element={<BookTable/>}/>
+  <Route path="/" element={<Login/>}/>
+  </Routes></BrowserRouter>
   );
 }
 
